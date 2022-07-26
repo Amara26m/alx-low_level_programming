d471bbcd3536f7288b69c265de7558570ad69f11
Amanda Arnandlall <agmtours786@gmail.com>
2022-07-26 13:02:25 -0700

    task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,5 +2,7 @@
 
 #include <stdio.h>
 void positive_or_negative(int i);
-int largest number (int a, int b, int c);
+int largest_number (int a, int b, int c);
+void printf_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 #endif
