6c6625053d19bcbaa57aea76accb5ad012751a4e
Amanda Arnandlall <agmtours786@gmail.com>
2022-08-08 17:06:57 -0700

    task 0

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
-/* _memset - divie
-* @s: pointer to char params
-* @b: data to change
-* @n: index
-* return: *s
+/**
+ *  _memset - fills memory with a constant byte.
+ * @s: source string
+ * @b: the constant byte for filling
+ * @n: length of buffer
+ * Return: new string.
 */
+char *_memset(char*s, char b, unsigned int n) 
+{
+	unsigned int i = 0;
 
-char *_memset(char*s, char b, unsigned int n) {
-      unsigned int i;
-
-      for (i = 0; i < n; i++)
-      {
-        s[i] = b;
-      }
-
-      return(s);
-
+	while (i < n)
+	{
+	         *(s + i) = b;
+	         i++;
+	}	 
+        return(s);
 }
