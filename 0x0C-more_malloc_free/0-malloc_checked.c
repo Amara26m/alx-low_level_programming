ebeac54e0fbb9b4083f030c6097bed5f9ae69612
Amanda Arnandlall <agmtours786@gmail.com>
2022-08-09 05:52:17 -0700

    mandatory tasks

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked-> checking malloc
- *@b: param b
+ * malloc_checked - checking malloc
+ * @b: parameter b
+ * Return: p.
  */
-void *malloc_checked(unsigned int b);
+
+void *malloc_checked(unsigned int b)
 {
 	void *p;
 
-	p = mallock (b);
-	if (p ==NULL)
-	|	exit(98);
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
 	return (p);
-
 }
